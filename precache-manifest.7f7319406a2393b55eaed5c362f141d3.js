5ec8906882e066569333097934ecb0e57e7907cd
Adriano Oliveira <drprado2@gmail.com>
2020-10-04 10:28:30 -0300

    Updates

---
+++ precache-manifest.7f7319406a2393b55eaed5c362f141d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adcebef98fd4c8a347dba2bf50f5fb6e",
+    "url": "/github-explorer/index.html"
+  },
+  {
+    "revision": "4ae11f0512a766af1939",
+    "url": "/github-explorer/static/js/2.aa504bf0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-explorer/static/js/2.aa504bf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee92c3d454ed2922587",
+    "url": "/github-explorer/static/js/main.4ec2fc6e.chunk.js"
+  },
+  {
+    "revision": "b76f610d1055037be066",
+    "url": "/github-explorer/static/js/runtime-main.efd11135.js"
+  }
+]);
